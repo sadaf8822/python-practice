9bb6dabe7e18e1b1f53554c2923b7e7f943368e8
sadaf8822 <134254337+sadaf8822@users.noreply.github.com>
2026-06-17 19:06:28 +0000

    Completed day-1 Python

---
+++ day_1.py
@@ -0,0 +1,18 @@
+# Operators and Datatypes
+# Operators
+print(2 + 4) # Addition Operator
+print(2 - 4) # Subtraction Operator
+print(2 * 4) # Multiplication Operator
+print(2 / 4) # Division Operator
+print(2 ** 4) # Exponent Operator
+print(2 % 4) # Modulus Operator
+print(2 // 4) # Floor Division Operator
+
+#Type Function -- Data types
+print(type(10)) #int
+print(type(3.22)) #float
+print(type(1+ 4j)) #complex
+print(type('Python')) #str
+print(type([1,2,3])) #list
+print(type({'name': 'Who'})) #dictionary
+print(type({9.8, 12.3, 22})) #set
