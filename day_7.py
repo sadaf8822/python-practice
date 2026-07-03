1a8619b346fe9fbfaa17d0fb4cae7fb613db637c
sadaf8822 <134254337+sadaf8822@users.noreply.github.com>
2026-07-03 19:13:36 +0000

    Topics covered: finding intersection items, checking superset, subset, difference between two sets, symmetric difference between two sets, disjoint sets

--- day_7.py
+++ day_7.py
@@ -44,3 +44,34 @@ negative_integers={-1,-2,-3,-4}
 integers=positive_integers| zero #Joining
 print(integers)
 print(integers.union(negative_integers)) #Joining
+
+#Intersection
+fruits={'Apricot', 'Apple', 'Mango', 'Banana'}
+fruits2={'Apricot', 'Apple'}
+common_fruits= fruits.intersection(fruits2)
+print("Result of intersection between two sets: ",common_fruits)
+
+#Superset and subset
+print("Is fruits a superset:", fruits.issuperset(fruits2)) #operator (fruits >= fruits2)
+print("Is fruits2 a subset:", fruits2.issubset(fruits)) #operator (fruits2 <= fruits)
+
+#Difference between two sets
+#Method-1
+st1={1,2,3,4}
+st2={5,6,7,8}
+print(st2-st1)
+#Method-2
+a={1,2,3,10,100}
+b={10,20}
+c={100, 200}
+print(a.difference(b,c)) #a-b= result, result-c
+
+#Symmetric Difference, finds unique elements across given sets
+set1 = {1, 2, 3}
+set2 = {2, 3, 4}
+set3 = {3, 4, 5}
+print(set1 ^ set2)
+print(set1 ^ set2 ^ set3)
+
+#Disjoint, having no common values
+print(set1.isdisjoint(set2))
