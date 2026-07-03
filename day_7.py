473ef0b3afe78eb9d0a0a0d9949c17a84d8d40e6
sadaf8822 <134254337+sadaf8822@users.noreply.github.com>
2026-07-03 18:39:41 +0000

    Topics covered: Removing an element from set, Clearing set, deleting set, converting a list into set and vice versa, joining set (using union operator and union function).

--- day_7.py
+++ day_7.py
@@ -18,3 +18,29 @@ new_berry1, new_berry2, new_berry3=input("Enter two to three berries:").split()
 berries.update([new_berry1, new_berry2, new_berry3])
 print(berries)
 
+berries.remove('raspberry')
+removed_berry=berries.pop()
+print(removed_berry)
+
+#berries.clear()
+#print(berries)
+
+del st
+
+#Converting list to set
+vegetables=['zucchini','onion', 'radish', 'eggplant', 'lady finger', 'bitter gourd']
+veg_set= set(vegetables)
+print("Datatype of vegetables list: ", type(vegetables), '\nDatatype of veg_set: ', type(veg_set))
+
+#Converting a set to list
+integers={1,2,3,4}
+integers_lt= list(integers)
+print("Datatype of integers set: ", type(integers), '\nDatatype of integers_lt: ', type(integers_lt))
+
+#Joining sets
+positive_integers={1,2,3,4}
+zero={0}
+negative_integers={-1,-2,-3,-4}
+integers=positive_integers| zero #Joining
+print(integers)
+print(integers.union(negative_integers)) #Joining
