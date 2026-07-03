0c816c1fd40938a2f138046302d2e1b42f949453
sadaf8822 <134254337+sadaf8822@users.noreply.github.com>
2026-07-03 12:04:30 +0000

    Converting list to tuple, and vice versa. Checking an element is present within tuple, joining a tuple, delete tuple.

--- day_6.py
+++ day_6.py
@@ -8,3 +8,28 @@ print('Last index value of tuple: ', spices[len(spices)-1])
 middle= len(spices)//2
 mid_item= spices[0:middle]
 print("Start to middle elements of spices tuple: ", mid_item)
+
+#Changing tuples to list
+cartoons=('Tom & Jerry', 'Oggy & The cockroaches', 'Toy story', 'Scooby Doo')
+tuple_list=list(cartoons) #Tuple to list
+print("The data type of cartoons tuple is:", type(cartoons), "\n The datatype of list tuple_list:", type(tuple_list))
+
+# List to tuple
+meat_food=["Chicken", "Fish", "Beef", "Quail", "Veal", "Mutton"]
+meats= tuple(meat_food)
+print("Datatype of meat_food list:", type(meat_food), "Datatype of meat tuple:", type(meats))
+
+#Checking an element exists in a tuple
+print("Does chicken exists in tuple:",'Chicken' in meats)
+
+#Joining Tuple
+color1= ('yellow', 'orange', 'green')
+color2=('blue', 'purple', 'indigo')
+colors=color1+color2
+print("Combining two tuples:", colors)
+
+#Deleting tuple
+del color1
+#print("After deletinng colors tuple: ",color1) #Generates name error as tuple is deleted
+
+
