8292114ca089d58f587bf808c3eec7bdd8e73afa
sadaf8822 <134254337+sadaf8822@users.noreply.github.com>
2026-07-04 11:23:29 +0000

    Second half topics covered: Changing a dictionary to a list of tuples (item method), Clearing a dictionary, deleting a dictionary, copy a dictionary, combining two dictionaries ( operator and update method), getting all keys of a dictionary, getting all values of a dictionary, getting a value associated to a key,

--- day_8.py
+++ day_8.py
@@ -37,3 +37,43 @@ last_value_pop= dct.popitem()
 print("Value popped:", value_popped, "\nLast value pop:", last_value_pop)
 del dct['key2']
 
+#Changes a dictionary into a list of tuples
+car={'brand': 'Ford', 'model': 'mustang', 'year': 2024}
+print(car.items())
+
+#Clearing a dictionary
+mlb_teams= {'Colorado': 'Rockies',
+ 'Minnesota': 'Twins', 
+ 'Seattle': 'Marriners', 
+ 'Milawakee': 'Brewers'
+ }
+print(mlb_teams.clear())
+
+#Deleting a dictionary
+del mlb_teams
+
+#Copy dictionary
+#Copy function can be used with list, dictionary, set. But not with tuple
+places={'USA': 'New York', 'Afghanistan': 'Kabul', 'England': 'London', 'Phillipine': 'Manila'}
+places_copy= places.copy()
+print(places_copy)
+
+#Combining two dictionaries together
+num1={"a":1, "b":2}
+num2={"c":3, "d":4}
+print(num1|num2)
+num1.update(num2) #Update function doesnot return
+print(num1)
+
+#Getting keys from a dictionary
+water_bodies= {"Amazon_River": "South_America", "Onega_Lake": "Europe", "Ganges_River": "Asia", "Baikal_Lake": "Euro_Asia"}
+keys=water_bodies.keys()
+print(keys)
+
+#Using values method to get values from a dictionary
+values=water_bodies.values()
+print(values)
+
+#Using get method-- get method gets the values of the key given as argument
+val= water_bodies.get("Amazon_River")
+print("Amazon river is located in continent: ",val)
