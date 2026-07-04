b907d16585476652b664b50759bcc22254fb3580
sadaf8822 <134254337+sadaf8822@users.noreply.github.com>
2026-07-04 07:40:45 +0000

    First task set complete: finding length, adding an element and adding multiple elements, remove function and their difference.

--- day_7.py
+++ day_7.py
@@ -75,3 +75,19 @@ print(set1 ^ set2 ^ set3)
 
 #Disjoint, having no common values
 print(set1.isdisjoint(set2))
+
+#Exercise
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+print('Length of set it companies: ', len(it_companies))
+it_companies.add("Twitter")
+new_companies= {'Intel', 'Spacex', 'Notion', 'OpenAI'}
+it_companies.update(new_companies)
+print("New elements updated in the set:", it_companies)
+it_companies.remove("Twitter")
+
+#Difference between remove and discard function is
+#discard function gives no error while the other gives keyerror msg
