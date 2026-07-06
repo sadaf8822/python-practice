b998377d7539e0f4047887ff9b432270938b0bd4
sadaf8822 <134254337+sadaf8822@users.noreply.github.com>
2026-07-06 09:33:35 +0000

    First task set completed: Compairing one input value with integer (must be older or equal to 18 to drive). Compairing two numerical inputs and displaying the difference between them(Difference between two ages entered, who is elder and how much elder). Using multiple conditions (to display grade achieved on the basis of marks entered). Compairing a string with lists (To guess which is the season on the basis of month entered by user).

--- day_9.py
+++ day_9.py
@@ -42,3 +42,55 @@ if user == 'admin' or access_level >=4:
     print("Welcome user:", user)
 else:
     print("Access Not granted")
+
+#Exercise Level1
+age= int(input("Enter your age:"))
+remaining_yrs= 18-age
+if age>=18:
+    print("You are old enough to drive")
+else:
+    print("You need {} more years to learn to drive" .format(remaining_yrs))
+
+my_age= int(input("Enter my age:"))
+your_age= int(input("Enter your age:"))
+difference_myage= my_age - your_age
+difference_yourage= your_age - my_age
+
+if my_age>your_age:
+    print("I am {} years older than you" .format(difference_myage))
+elif my_age == your_age:
+    print("We are of the same age")
+else:
+    print("You are {} years older than me" .format(difference_yourage))
+
+grade=int(input("Enter your marks: "))
+if grade==100 or grade >= 90:
+    print("90-100 grade: A")
+elif grade==89 or grade >= 80:
+    print("80-89 grade: B")
+elif grade==79 or grade >= 70:
+    print("70-79 grade: C")
+elif grade==69 or grade >= 60:
+    print("60-69 grade: D")
+elif grade==59 or grade >= 0:
+    print("0-59 grade: F")
+else:
+    print("Invalid marks entered") 
+
+month=str(input("Enter a month:"))
+autumn= ['September','september' ,'October','october' ,'November', 'november']
+winter=['December','February','January','december','february','january']
+spring=['March', 'April', 'May', 'march', 'april', 'may']
+summer=['June', 'July', 'August','june', 'july', 'august']
+
+if month in autumn:
+    print("It is autumn in {}" .format(month))
+elif month in winter:
+    print("It is winter in {}" .format(month))
+elif month in spring:
+    print("It is spring in {}" .format(month))
+elif month in summer:
+    print("It is summer in {}" .format(month))
+else:
+    print("Invalid entry")
+
